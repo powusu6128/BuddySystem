219f31cfddf65400fcb23b092ed4b755f82707e5
R. Smith <smithr1@al9.rowanads.rowan.edu>
2015-03-26 23:43:20 -0400

    Implement Allocation
    Implemented Allocation of memory.

--- src/MemoryManager.java
+++ src/MemoryManager.java
@@ -26,9 +26,6 @@ public class MemoryManager {
 		return maxMemorySize;
 	}
 	
-	public void allocateMemory(BlockOMemory process){
-		
-	}
 	
 	public void deallocateMemory(BlockOMemory process){
 		/**
@@ -53,6 +50,94 @@ public class MemoryManager {
         mergeMemory();
 
     }//end deallocate
+	
+	
+	
+		/**
+     * This method allows memory to be allocated using the Memory Buddy System.
+     * A list of available memory slots is checked to see if any memory is available.
+     * 
+     * @param size
+     * @param processSize
+     * @param isProcess
+     * @param buddy
+     * @param id 
+     */
+    private static void AllocateMemory(long size, long processSize, boolean isProcess, int buddy, int id)
+    {
+        boolean noSpaceAvailable = false;
+        if (processSize < minMemorySize)
+            System.out.println("Process size less than minimum size allowed.");
+        else if (processSize > (maxMemorySize / 2))
+            System.out.println("Process size greater than maximum size allowed");
+        else
+        {
+            boolean rounding = true;
+            int x = 0;
+            boolean sizeFound = false;
+            double closestAvailableMemorySize = 0;
+            //finds power of 2 closest and >= processSize
+            if (Functions.isPowerOfTwo(processSize))
+            {
+                x = Functions.logTwo(processSize);
+            }
+            else
+            {
+                x = Functions.logTwo(processSize) + 1;
+            }
+            //uses that power of 2 to look in freememory array for available space
+            while (!sizeFound)
+            {
+                if (Math.pow(2, x) > (maxMemorySize / 2))
+                {
+                    noSpaceAvailable = true;
+                    break;
+                    //have exceeded maximum space
+                }
+                else if (freeMemory[x] > 0)
+                {
+                    sizeFound = true;
+                }
+                else
+                {
+                    x++;
+                }
+            }
+            if (!noSpaceAvailable)
+            {
+                boolean memoryFound = false;
+                int i = 0;
+                while(!memoryFound)
+                {
+                  if (memoryBlocks.get(i).getSize() == processSize && !memoryBlocks.get(i).isProcess())
+                    {
+                        if (processSize < (memoryBlocks.get(i).getSize() / 2))
+                        {
+                            //Split and make babies!!!
+                            int blockSize = memoryBlocks.get(i).getSize();
+                            memoryBlocks.add(i+1, new BlockOMemory((blockSize/2),0,false,i,null));
+                            memoryBlocks.get(i).setSize(blockSize/2);
+                        }
+                        else
+                        {
+                            memoryFound = true;
+                            memoryBlocks.get(i).setProcessSize(processSize);
+                            memoryBlocks.get(i).setIsProcess(true);
+                            memoryBlocks.get(i).setProcessID(id);
+                            freeMemory[x] = freeMemory[x] + 1;
+                        }
+                     }
+                }
+            }
+            else
+            {
+                System.out.println("No Space available to add this process.");
+            }
+        }
+    }
+
+
+	
 	
 	
 	private void mergeMemory()
